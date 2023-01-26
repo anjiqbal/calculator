23bf5744bf8a6bf904d7531967351b7b291d56c3
Anjum Iqbal <anjum.r.iqbal@gmail.com>
2023-01-26 13:46:16 +0000

    updated css

--- main.js
+++ main.js
@@ -1,19 +1,16 @@
-// selects all elements with a class of number and assigns it to a variable called numbers
+
 const numbers = document.querySelectorAll('.number')
 
-// selects all elements with a class of operator and assigns it to a variable called operators
 const operators = document.querySelectorAll('.operator')
 
 document.querySelector('#equals').addEventListener('click', equals)
 
 document.querySelector('#decimal').addEventListener('click', decimal)
 
-// selects the element with the id equals and adds an event listener 
 document.querySelector('#equals').addEventListener('click', equals)
 
 document.querySelector('.clear'). addEventListener('click', clear)
 
-//use forEach to iterate over the node lists and add an event listener to each that activates on click and calls a function
 numbers.forEach(num => {
     num.addEventListener('click', display)
 })
@@ -21,9 +18,7 @@ numbers.forEach(num => {
 operators.forEach(operator => {
     operator.addEventListener('click', operatorDisplay)
 })
-let num1 = ''
-let num2 = ''
-//assigns the text content of the calling button to a variable which is then added to the h2
+
 function display() {
     let onScreenDisplay = document.querySelector('h2')
     let text = this.textContent
@@ -31,7 +26,6 @@ function display() {
 }
 
 
-//adds the text content of the calling button to the display and then removes the event listeners from all operators so that they can only be used once
 function operatorDisplay() {
     let onScreenDisplay = document.querySelector('h2')
     let minDisplay = document.querySelector('h3')
